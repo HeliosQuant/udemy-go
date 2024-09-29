c4d44408b85348b706dc3836f874001f54ec3a11
Kenja Koa <othmane.fathallah.1@outlook.com>
2024-09-29 20:34:14 +0100

    Multiple Return Values

--- cards/main.go
+++ cards/main.go
@@ -1,10 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
-	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	hand, remainingCards := deal(cards, 5)
+	hand.print()
+	fmt.Println("================")
+	remainingCards.print()
 }
