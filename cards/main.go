b880a2e27b2efd9c9b26b4c61ba517e3483e9afa
Kenja Koa <othmane.fathallah.1@outlook.com>
2024-10-05 22:48:55 +0100

    Error Handling

--- cards/main.go
+++ cards/main.go
@@ -1,14 +1,11 @@
 package main
 
-import "fmt"
-
 func main() {
 	// cards := newDeck()
 	// hand, remainingCards := deal(cards, 5)
 	// hand.print()
 	// fmt.Println("================")
 	// remainingCards.print()
-	cards := newDeck()
-	a := cards.saveToFile("my_cards")
-	fmt.Println(a)
+	cards := newDeckFromFile("my_cards")
+	cards.print()
 }
