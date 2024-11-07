c28174ffdffef19a1c39ee1f42f0c3d7e558c446
Helios Quant <heliosquant@gmail.com>
2024-11-06 20:40:41 -0800

    structs with receiver functions

--- structs/main.go
+++ structs/main.go
@@ -8,21 +8,28 @@ type contactInfo struct {
 }
 
 type person struct {
-	firstName string
-	lastName  string
-	contact   contactInfo
+	firstName   string
+	lastName    string
+	contactInfo // same as 'contact   contactInfo' in struct, it's called anonymous field
 }
 
 func main() {
 	jim := person{
 		firstName: "Jim",
 		lastName:  "Party",
-		contact: contactInfo{
+		contactInfo: contactInfo{
 			email:   "jim@gmail.com",
 			zipCode: 123456,
 		},
 	}
+	jim.updateName("Jimmy")
+	jim.print()
+}
 
-	fmt.Println(jim)
-	fmt.Printf("%+v", jim)
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
