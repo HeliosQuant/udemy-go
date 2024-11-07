6260a8d849775f55101db6d4c098f84c93c40576
Helios Quant <heliosquant@gmail.com>
2024-11-07 13:15:38 -0800

    use make function for map

--- map/main.go
+++ map/main.go
@@ -3,11 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var colors map[string]string
+	// var colors map[string]string
+	colors := make(map[int]string)
 	// colors := map[string]string{
 	// 	"red":   "#FF0000",
 	// 	"green": "#00ff00",
 	// }
 
+	colors[10] = "#FFFFFF"
+
+	fmt.Println(colors)
+
+	delete(colors, 10) // built-in function dedicated for deleteing map element
 	fmt.Println(colors)
 }
