faceecce9658b7ca54efaa840c4f9ce7bc5593a4
Helios Quant <heliosquant@gmail.com>
2024-11-12 22:31:32 -0800

    Repeating Routines

--- channels/main.go
+++ channels/main.go
@@ -21,8 +21,8 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	for i := 0; i < len(links); i++ {
-		go fmt.Println(<-c)
+	for {
+		go checkLink(<-c, c)
 	}
 }
 
@@ -30,9 +30,9 @@ func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link + " must be down.")
-		c <- "Might be down I think"
+		c <- link
 		return
 	}
 	fmt.Println(link + " is up.")
-	c <- "Yep it's up"
+	c <- link
 }
